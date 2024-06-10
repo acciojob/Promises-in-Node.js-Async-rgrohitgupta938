af52b4bbb53b21fa885a66fdc2b21ea4f1de981f
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-06-11 00:15:12 +0530

    first

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const filePath = process.argv[2];
 
 function readFileAsync(filePath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
       if (err) {
         reject(`Error reading file ${filePath}: ${err}`);
         return;
@@ -17,6 +17,12 @@ function readFileAsync(filePath) {
 function modifyText(text) {
   return new Promise((resolve, reject) => {
     // TODO: Convert text to uppercase and reverse it
+    if (!text || typeof text !== "string") {
+      reject("Invalid text");
+      return;
+    }
+    const modiText = text.toUpperCase().split("").reverse().join("");
+    resolve(modiText);
   });
 }
 
